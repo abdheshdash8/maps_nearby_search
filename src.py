af4627ecdb032749a194e59b2f23508a759f4b5b
abdheshdash8 <97735552+abdheshdash8@users.noreply.github.com>
2024-06-30 11:58:13 +0800

    Update src.py

--- src.py
+++ src.py
@@ -1,254 +1,121 @@
-############################# Code for sorting a list of tuples by mergeSort method. #################################
-# Code for merging 2 sorted lists of tuples.
-
-
-def merge(L1, L2, ind):
-    # Given two sorted lists L1 and L2, return a new sorted list L3
-    # ind is the index of the tuple that is to be sorted.
-    L3 = []
-    m = len(L1)
-    n = len(L2)
-    i = 0
-    j = 0
-    while (i < m) & (j < n): # in each iteration either i or j increases by 1
-        if L1[i][ind] <= L2[j][ind]: # find the lower of L[i] and L[j] to copy over to L3
-            L3.append(L1[i])
-            i += 1
-        else:
-            L3.append(L2[j])
-            j+= 1
-    if (i == m):
-        while (j < n): # Copy remaining L2 elements
-            L3.append(L2[j])
-            j+= 1
-    else:
-        while (i < m): # Copy remaining L1 elements
-            L3.append(L1[i])
-            i += 1
-    return L3 # O(m+n)
-
-# Code for merge sort for a list of tuples.
-def mergeSort(L, ind):
-    # INPUT L : any list (L s.t. True)
-    # ind is the index of the tuple that is to be sorted.
-    # OUTPUT L3 is a permutation of L, L3 is sorted
-    n = len(L)
-    if (n <= 1 ):   # L is already sorted
-        return L
-    else:
-        mid = n//2      # find mid point of list
-        # Divide the list in 2 roughly equal parts,
-        L1 = L[0:mid]   # first half
-        L2 = L[mid:n]   # second half
-        # recursively sort first half # T(n//2)
-        L1sort = mergeSort(L1, ind)
-        # recursively sort second half # T(n//2)
-        L2sort = mergeSort(L2, ind)
-        # merge sorted lists O(n//2 + n//2)
-        L3 = merge(L1sort, L2sort, ind)
-        # T(n) = 2 T(n//2) + O(n)
-        # Hence, T(n) = n*log(n)
-    return L3
-
-
-
 class Node:
-    # Construction of a node.
-    def __init__(self, key) -> None:
-        self.cut_value = key
+    def __init__(self, point):
+        self.point = tuple(point)
         self.leftChild = None
         self.rightChild = None
-        self.is_leaf = False
-        self.ytree = None   # Its the associated y-tree with the node.
+        self.associatedStructure = None
 
-
-def build1DRangeTree(set_of_points):
-    # It builds a 1D range tree.
-    # Input:
-    # set_of_points = the collection of points that are to be stored in the tree.
-    # Output:
-    # Root node of the tree.
-    if not set_of_points:
-        return None
-    elif len(set_of_points) == 1:
-        _Node1 = Node(set_of_points[0])
-        _Node1.is_leaf = True
-        return _Node1
-    else:
-        mid = len(set_of_points)//2
-        _Node = Node(set_of_points[mid])
-        _Node.leftChild = build1DRangeTree(set_of_points[:mid])
-        _Node.rightChild = build1DRangeTree(set_of_points[(mid+1):])
-        return _Node
-
-
-def build2DRangeTree(set_of_points, prefer_x = True):
-    # It builds a 2D range tree.
-    # Input:
-    # set_of_points = the collection of points that are to be stored in the tree.
-    # prefer_x = It is True if you have to build x-tree else False.
-    # Output:
-    # Root node of the tree.
-    if not set_of_points:
-        return None
-    if len(set_of_points) == 1:
-        node = Node(set_of_points[0])
-        node.is_leaf = True
-    else:
-        mid = len(set_of_points)//2
-        node = Node(set_of_points[mid])
-        node.leftChild = build2DRangeTree(set_of_points[:mid], prefer_x)
-        node.rightChild = build2DRangeTree(set_of_points[(mid+1):], prefer_x)
-    if prefer_x:
-        node.ytree = build2DRangeTree(set_of_points.sort(key = lambda x : x[1]), prefer_x = False)
-        return node
-
-
-def checkInRange(point, range, dimension):
-    # Input:
-    # point = a point from the set of points.
-    # range = the range of allowed values of x and y.
-    # dimension = the dimension of the point i.e. 1D or 2D.
-    # Output:
-    # It returns True id the point is in the range else False.
-    if dimension == 1:
-        x = point
-        if (x >= range[0][0]  and x <= range[0][1] ) :
-            return True
-        else:
-            return False
-    elif dimension == 2:
-        x = point[0]
-        y = point[1]
-
-        if (x >= range[0][0]   and x <= range[0][1]  and y >= range[1][0]  and y <= range[1][1] ) :
-            return True
-        else:
-            return False
-
-
-def getValue (point, prefer_x, dimension ):
-    # Input:
-    # point = a point from the set_of_points.
-    # prefer_x = True if you have to get the value of x- coord, else False.
-    # dimension = the dimension of the point i.e. 1D or 2D.
-    if dimension == 1:
-        value = point.cut_value
-    elif dimension == 2:
-        if prefer_x:
-            value = point.cut_value[0]
-        else:
-            value = point.cut_value[1]
-    return value
-
-
-
-def findSplitNode(root, lower, upper, dimension , prefer_x ):
-    # Input:
-    # root = root node of the tree.
-    # lower = the lower limit of the range.
-    # upper = the upper limit of the range.
-    # dimension = the dimension of the point i.e. 1D or 2D.
-    # prefer_x = prefer_x = True if you have to get the value of x- coord, else False.
-    splitnode = root
-    while splitnode != None:
-        node = getValue(splitnode, prefer_x, dimension)
-        if upper < node:
-            splitnode = splitnode.leftChild
-        elif lower > node:
-            splitnode = splitnode.rightChild
-        elif lower <= node <= upper :
-            break
-    return splitnode 
-
-
-def search1DRangeTree(node, lower, upper, dimension, prefer_x = True):
-    # Input:
-    # node = a node in the tree.
-    # lower = the lower limit of the range.
-    # upper = the upper limit of the range.
-    # dimension = the dimension of the point i.e. 1D or 2D.
-    # prefer_x = prefer_x = True if you have to get the value of x- coord, else False.
-    # Output:
-    # Searches for the points in range in a 1D tree.
-    results1 = []
-    splitnode = findSplitNode(node, lower, upper, dimension, prefer_x)
-    if splitnode == None:
-        return results1
-    # Check if the node is a valid node in range
-    elif checkInRange(getValue(splitnode, prefer_x, dimension) , [(lower, upper)], 1):
-        results1.append(splitnode.value)
-    # search for nodes in left subtree
-    results1 += search1DRangeTree(splitnode.left, lower, upper,dimension, prefer_x)
-    # search for nodes in right subtree
-    results1 += search1DRangeTree(splitnode.right, lower, upper, dimension, prefer_x)
-    return results1
-
-
-def search2DRangeTree(tree, x1, x2, y1, y2, dimension, results = []):
-    # Input:
-    # tree = a node in the tree.
-    # lower = the lower limit of the range.
-    # upper = the upper limit of the range.
-    # dimension = the dimension of the point i.e. 1D or 2D.
-    # prefer_x = prefer_x = True if you have to get the value of x- coord, else False.
-    # results is the result list.
-    # find the node which the least common ancestor in the tree for given range.
-    # Output:
-    # Searches for the points in range in a 2D tree.
-    splitnode = findSplitNode(tree, x1, x2, 2, True)
-    if (splitnode == None):
-        return results
-    elif checkInRange(splitnode.cut_value, [(x1, x2), (y1, y2)], 2) :
-        results.append(splitnode.cut_value)
-        # Traverse the nodes in left child of split node
-        l = splitnode.leftChild 
-        while ( l != None ):
-            # Check if the node is a valid node in range
-            if checkInRange(l.cut_value, [(x1, x2), (y1, y2)], 2):
-                results.append(l.cut_value)
-            # Search the associated ytree at the left child of current node in xtree
-            if (x1 <= l.cut_value[0]):
-                if l.rightChild != None:
-                    results += search1DRangeTree(l.rightChild.ytree, y1, y2, dimension, False)
-                l = l.leftChild
-            else:
-                l = l.rightChild
-
-        # Traverse the nodes in left child of split node
-        r = splitnode.rightChild
-        while ( r != None ):
-            # Check if the node is a valid node in range
-            if checkInRange(r.cut_value, [(x1, x2), (y1, y2)], 2):
-                results.append(r.cut_value)
-            # Search the associated ytree at the left child of current node in xtree
-            if ( x2 >= r.cut_value[0] ):
-                if r.leftChild != None:
-                    results += search1DRangeTree(r.leftChild.ytree, y1, y2, dimension, False)
-                r = r.rightChild
-            else:
-                r = r.leftChild
-        
-        return results
-    else:
-        search2DRangeTree(tree.leftChild, x1, x2, y1, y2, dimension, results)
-        search2DRangeTree(tree.rightChild, x1, x2, y1, y2, dimension, results)
-        return results
 
 class PointDatabase:
-    # Node class for making nodes for range tree that is used below. 
     def __init__(self, pointlist):
-        global RangeTree
-        RangeTree = build2DRangeTree(pointlist, True)
-    def searchNearby(self, q, d):
-        return search2DRangeTree(RangeTree, q[0]-d, q[0]+d, q[1]-d, q[1]+d, 2)
-
-pointDbObject = PointDatabase([(1,6), (2,4), (3,7), (4,9), (5,1), (6,3), (7,8), (8,10),(9,2), (10,5)])
-print(pointDbObject.searchNearby((4,8), 5))
-
-            
+        pointlist_x = sorted(pointlist)
+        pointlist_y = sorted(pointlist, key=lambda y: y[1])
+        self.root = self.RangeTree(pointlist_x, pointlist_y)
 
     
+    def RangeTree(self, pointlist_x, pointlist_y):
+        if not pointlist_x:
+            return None
+        
+        #Partition points around median x-coordinate
+        medianIdx = (len(pointlist_x)-1)//2
+        median = pointlist_x[medianIdx][0]
+        pointlist_x_l = pointlist_x[:medianIdx]
+        pointlist_x_r = pointlist_x[medianIdx+1:]
 
+        # split list in Linear Time while maintaing ordering w.r.t. y
+        pointlist_y_l = [y for y in pointlist_y if y[0] < median]
+        pointlist_y_r = [y for y in pointlist_y if y[0] > median]
+
+        #recurse on partitions
+        leftChild = self.RangeTree(pointlist_x_l, pointlist_y_l)
+        rightChild = self.RangeTree(pointlist_x_r, pointlist_y_r)
+
+        root = Node(pointlist_x[medianIdx])
+        root.leftChild = leftChild
+        root.rightChild = rightChild
+        root.associatedStructure = pointlist_y # Store a list of points in subtree sorted by y coordinate at each node
+        return root
+
+    #check whether the given node falls within desired range
+    def within_bounds(self, node, q, d):
+        x, y = node.point
+        return max(abs(x-q[0]), abs(y-q[1])) < d
+
+    #Binary search the least value greater than or equal to v in sorted pointlist
+    def upper_bound(self, l, v):
+        lo, hi = 0, len(l) - 1
+        while lo <= hi:
+            mid = (lo+hi)//2
+            if l[mid][1] < v:
+                lo = mid+1
+            else:
+                hi = mid-1
+        return lo
+
+    #Filter the list to contain all points satisfying ymin <= y < ymax
+    def filter_list(self, l, q, d):
+        ymin = q[1]-d
+        ymax = q[1]+d
+        return l[self.upper_bound(l, ymin):self.upper_bound(l, ymax)]
         
+    #Main function for range queries
+    def search_tree(self, node, q, d, lbound = float('-inf'), rbound = float('inf')):
+        #Base case
+        if not node:
+            return []
+        xmin, xmax = q[0]-d, q[0]+d
+        x, y = node.point
+        # print("Node", node.point, '\n', xmin, xmax, lbound, rbound)
+        #If node's bounds lie within desired range, filter directly on y
+        if lbound > xmin and rbound < xmax:
+            # print("case 1\n")
+            return self.filter_list(node.associatedStructure, q, d)
+        #Go right
+        elif x < xmin:
+            # print("case 2\n")
+            #Right subtree cannot contain points to the left of parent
+            return self.search_tree(node.rightChild, q, d, x, rbound)
+        #Go left
+        elif x > xmax:
+            # print("case 3\n")
+            #Left subtree cannot contain points to the right of parent
+            return self.search_tree(node.leftChild, q, d, lbound, x)
+        #Must recurse on both subtrees
+        else:
+            # print("case 4\n")
+            #Check whether parent lies in desired range
+            points = [node.point] if self.within_bounds(node, q, d) else []
+            points.extend(self.search_tree(node.leftChild, q, d, lbound, x))
+            points.extend(self.search_tree(node.rightChild, q, d, x, rbound))
+            return points
+
+    #Search within whole tree
+    def searchNearby(self, q, d):
+        return self.search_tree(self.root, q, d) 
+
+    
+    # test
+    # def inorder(self, root, list1):
+    #     if root == None:
+    #         return 
+    #     self.inorder(root.leftChild, list1)
+    #     list1.append([root.point])
+    #     self.inorder(root.rightChild, list1)
+
+    # def preorder(self, root, list1):
         
+    #     if root == None:
+    #         return 
+    #     list1.append([root.point])
+    #     self.preorder(root.leftChild, list1)
+    #     self.preorder(root.rightChild, list1)
+
+
+
+
+if __name__ == '__main__':
+    obj =  PointDatabase([[1,6], [2,4], [3,7], [4,9], [5,1], [6,3], [7,8], [8,10], [9,2], [10,5]])
+    print(obj.searchNearby((7,6), 3.5))
+    print(obj.searchNearby((4,8), 5))
+
